2cd908d9a0436a47d0739eeba8599fb9211a7f18
Munar <karymshakovmunar@gmail.com>
2021-08-25 18:58:01 +0600

    realization category and add new categories

---
+++ src/components/CategoryItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <a href="#" :class="classes">{{category}}</a>
+</template>
+
+<script>
+
+
+export default {
+  props:['category','isActive'],
+
+computed:{
+  classes() {
+    const classes = ['px-3',' py-1', 'transition'  ,'border' , 'rounded-full']
+  return this.isActive
+  ? [...classes,'hover:bg-gray-500', 'text-white' ,'bg-gray-600', 'border-gray-700']:[...classes,'hover:bg-gray-200', 'bg-gray-100', 'border-gray-300']
+    }
+}
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -130,122 +130,7 @@
               d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
           ></path>
         </svg>
-        <div
-            class="opacity-0 group-hover:opacity-100 absolute top-9 right-0 sm:left-0 bg-white w-60 border border-t-0"
-        >
-          <section class="py-2 border-b">
-            <ul>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      class="w-6 h-6 mr-3 text-red-500"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                        clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>YouTube TV</span>
-                </a>
-              </li>
-            </ul>
-          </section>
-          <section class="py-2 border-b">
-            <ul>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      class="w-6 h-6 mr-3 text-red-500"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                        clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>YouTube Music</span>
-                </a>
-              </li>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      class="w-6 h-6 mr-3 text-red-500"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                        clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>YouTube Kids</span>
-                </a>
-              </li>
-            </ul>
-          </section>
-          <section class="py-2">
-            <ul>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      class="w-6 h-6 mr-3 text-red-500"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                        clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>Creator Academy</span>
-                </a>
-              </li>
-              <li>
-                <a
-                    href="#"
-                    class="flex items-center px-3 py-2 text-sm hover:bg-gray-100"
-                >
-                  <svg
-                      class="w-6 h-6 mr-3 text-red-500"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                        fill-rule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                        clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>YouTube for Artists</span>
-                </a>
-              </li>
-            </ul>
-          </section>
-        </div>
+       <TheDropdown />
       </button>
       <button class="relative group p-2 focus:outline-none">
         <svg
@@ -507,3 +392,9 @@
     </div>
   </header>
 </template>
+<script>
+import TheDropdown from "./TheDropdown.vue";
+export default {
+  components: {TheDropdown}
+}
+</script>
